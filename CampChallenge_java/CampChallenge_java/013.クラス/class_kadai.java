aef72e3da5d1f73759c5033b240feeba0ea71c34
EXP-0002\guest1Day <oohamatakuto71@gmail.com>
2018-02-09 18:41:20 +0900

    クラスの継承課題提出

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/class_kadai.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class_kadai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class class_kadai extends HttpServlet {
+      public int i=0;
+      public int j=0;
+      public int getTax(int i){
+          return i;
+      }
+      public int getTAX(int j){
+          return j;
+      }
+      public void kousin(int i,int j){
+          this.i=i;
+          this.j=j;
+      }
+      public void gamen(int i,int j){
+          this.i=i;
+          this.j=j;
+      }
+      public void kuria(int i,int j){
+          this.i=i;
+          this.j=j;
+      }
+      public void prin( PrintWriter out){
+          out.println("1番目の変数は"+(this.i=i)+"です");
+          out.println("2番目の変数は"+(this.j=j)+"です");
+      }
+      
+      
+      
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+    //繝｡繧ｽ繝�繝画峩譁ｰ,蜻ｼ縺ｳ縺�縺�
+    kousin(10,13);
+    gamen(10,13);
+    kuria(0,0);
+    prin(out);
+        }
+    }
+    
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
