c4ddc39e89ccd5608dcd12dad31599292e41de6d
EXP-0002\guest1Day <oohamatakuto71@gmail.com>
2018-02-13 13:33:51 +0900

    引数戻り値1修正提出

--- CampChallenge_java/CampChallenge_java/011.メソッド/引数戻り値1.java
+++ CampChallenge_java/CampChallenge_java/011.メソッド/引数戻り値1.java
@@ -31,9 +31,9 @@ public class 引数戻り値1 extends HttpServlet {
         String[] name = {"1001<br>", "私の名前は大濱拓人です<br>", "1993年6月2日<br>", "東京都江戸川区<br>"};
         String[] n = {"1002<br>", "私の名前は羽田圭介です<br>", "1985年10月19日<br>", "埼玉県北葛飾群<br>"};
         String[] na = {"1003<br>", "私の名前は林修です<br>", "1965年9月2日<br>", "愛知県名古屋市<br>"};
-        if (ID == "1001") {
+        if (ID .equals ("1001")) {
             return name;
-        } else if (ID == "1002") {
+        } else if (ID.equals ("1002")) {
             return n;
         } else {
             return na;
@@ -46,21 +46,21 @@ public class 引数戻り値1 extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            int k;
+            int o;
             String ID = "1001";
-            for (int o = 1; o <= 3; o++) {
+            for (o = 1; o <= 3; o++) {
 
                 String[] j = myprofile(ID, out);
 
-                for (int k = 1; k <= 3; k++) {
+                for (k = 1; k <= 3; k++) {
                     out.print(j[k]);
                 }
-                
-                if (ID == "1001") {
+                if (ID .equals("1001")) {
                     ID = "1002";
 
-                } else if (ID == "1002") {
+                } else if (ID .equals("1002")) {
                     ID = "1003";
-
                 }
 
             }
