35c81107bfec23794f32bc7e8397ddc7e3dd6750
EXP-0002\guest1Day <oohamatakuto71@gmail.com>
2018-02-19 19:29:20 +0900

    セッションの作成を提出

---
+++ CampChallenge_java/CampChallenge_java/016.データ操作/kofulu.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class kofulu extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        Date d = new Date(); // 現時刻の取得
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm;ss"); // 日付フォーマット
+        HttpSession hs = request.getSession(); // セッションを呼んでくる
+        String timeStamp = (String)hs.getAttribute("time"); // タイムスタンプをセッションから取得
+        hs.setAttribute("time",sdf.format(d));
+        try (PrintWriter out = response.getWriter()) {
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>セッションによるアクセス時間表示</title>");            
+            out.println("</head>");
+            out.println("<body bgcolor=\"f5deb3\">");
+            out.println("<h1>セッション利用確認ページ</h1>");
+            if(timeStamp != null){
+                out.println("このページには前回" + timeStamp + " にアクセスしています。");
+            }else{
+                out.println("アクセスが初回のようです。次回からタイムスタンプが表示されます");
+            }
+            out.println("<br>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
