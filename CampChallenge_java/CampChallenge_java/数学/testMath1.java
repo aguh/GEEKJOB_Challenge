338e698c1f84f6e3482a95c8baab5760054ec6dd
EXP-0002\guest1Day <oohamatakuto71@gmail.com>
2018-03-14 14:47:18 +0900

    絶対値を表示

---
+++ CampChallenge_java/CampChallenge_java/数学/testMath1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+class testMath1{
+  public static void main(String args[]){
+    for (int i = -5 ; i < 5 ; i++){
+      System.out.println(i + "‚Ìâ‘Î’l‚Í" + Math.abs(i) + "‚Å‚·");
+      
+    }
+  }
+}
